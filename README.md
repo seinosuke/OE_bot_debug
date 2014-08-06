a949db3e9098159d08890f5e7bf9412a3ac845f6
seinosuke <seinosuke.3606@gmail.com>
2014-08-06 11:07:33 +0900

    entry.rb -> access.rb

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 ### 機能
 - 「だれかおるか？」、「誰かいますか」などとリプを送ると3L502にいる人がわかります。   
 - 学生証を登録すれば部屋にあるPaSoRiにかざすだけで入退室の報告ができます。   
-- 「きしつ」「機室」を含むリプを送ると3L504で何台PCが稼働中であるかわかります。   
+- 「きしつ」「機室」を含むリプを送ると、3L504で何台PCが稼働中であるかわかります。   
+- 「L棟パンガチャ」とリプを送ると、L棟２階ラウンジにあるパンの中からランダムでオススメを返します。   
 
 ### 謝辞
-[ktansai](https://github.com/ktansai)さんの[esysPinger](https://github.com/ktansai/esysPinger)を使用させていただきました。ありがとうございます。
+- [ktansai](https://github.com/ktansai)さんの[esysPinger](https://github.com/ktansai/esysPinger)を使用させていただきました。ありがとうございます。   
+- bunkai_freeさんにL棟パンガチャを提供していただきました。ありがとうございます。   
--- access.rb
+++ access.rb
@@ -6,29 +6,58 @@ oebot = Bot.new()
 card = Card.new()
 list = Array.new()
 i = 0
+input = ""
 
 loop do
   begin
-    # カードを読むまで諦めない
-    num = idnum()
-    # members.csv にないIDが読み込まれた場合guest(数字)と表示される
-    if card.hash(num).nil? then
-      card.reload_guest(num,"guest(#{i})")
-      i += 1
-    end
-    card.reload # members.csv から
-    list << card.hash(num)
+    print "ฅ(๑'Δ'๑) あなたは既に登録されてますか(y/n)? :"
+    input = gets.to_s.chomp
 
-    # 重複した人物は退室
-    leaver = list.uniq.select{|i| list.index(i) != list.rindex(i)}
-    if !(leaver.empty?) then
-      list = list - leaver
-      oebot.out(list,leaver)
+    if input == "y" || input == "Y" then
+      puts "ฅ(๑'Δ'๑) カードを置いてください。"
+      # カードを読むまで諦めない
+      num = idnum()
+      # members.csv にないIDが読み込まれた場合guest(数字)と表示される
+      if card.hash(num).nil? then
+        card.reload_guest(num,"guest(#{i})")
+        i += 1
+      end
+      card.reload # members.csv から
+      list << card.hash(num)
+      # 重複した人物は退室
+      leaver = list.uniq.select{|i| list.index(i) != list.rindex(i)}
+      if !(leaver.empty?) then
+        list = list - leaver
+        oebot.out(list,leaver)
+      else
+        oebot.in(list,card.hash(num))
+      end
+      sleep 3
+
+    elsif input == "n" || input == "N" then
+      print "ฅ(๑'Δ'๑) 名前を入力してください："
+      name = gets.chomp!.to_s
+      puts "ฅ(๑'Δ'๑) カードを置いてください"
+      id_num = idnum()
+      file_name = "./list/members.csv"
+      new_member = "#{id_num},#{name}\n"
+      entry = File.open(file_name,"a")
+      entry.write(new_member)
+      entry.close
+      puts "(๑¯Δ¯๑)/ 登録が完了しました!\n"
+      sleep 3
+
     else
-      oebot.in(list,card.hash(num))
+      puts "(๑¯Δ¯๑)/ もういちど入力してください。\n"
+      redo
     end
+    input = ""
+
+  rescue Interrupt # ctrl + C
+    exit 1
+  rescue => em
+    p em
     sleep 3
-    rescue Interrupt # ctrl + C
-      exit 1
+    retry
   end
 end
